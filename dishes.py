d12abe83c014372013d9f9f0873fa6b5a4356ac0
Vira Buniak <virynka.b@gmail.com>
2017-04-29 22:29:56 +0300

    Initial commit. Draft

---
+++ dishes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask('recipes')
+mongo = PyMongo(app)
+
+@app.route('/')
+def home_page():
+    dishes_cursor = mongo.db.dishes.find({}, {'_id': 0})
+    dishes = [item for item in dishes_cursor]
+    return render_template('index.html', dishes=dishes)
+
+
+if __name__ == "__main__":
+    app.run()
